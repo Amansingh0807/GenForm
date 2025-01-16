a38374c28d8ea77ee22a9becb8627d0af7f9fea3
Aman Singh <amansingh080704@gmail.com>
2025-01-16 20:26:45 +0530

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = ( props: Props) =>{
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
                     <a
                       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                      href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                      href="https://github.com/Amansingh0807"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -24,7 +24,7 @@ const Footer = ( props: Props) =>{
                     </a>
                     <a
                       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                      href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                      href="https://www.linkedin.com/in/amansingh08/"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -39,7 +39,7 @@ const Footer = ( props: Props) =>{
                     </a>
                     <a
                       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                      href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                      href="https://x.com/RealAman_Singh"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
