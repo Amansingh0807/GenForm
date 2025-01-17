fdb458ff985cac57d22c7c36f20d8f7965f36942
Aman Singh <amansingh080704@gmail.com>
2025-01-17 12:28:48 +0530

    final

--- app/dashboard/upgrade/page.tsx
+++ app/dashboard/upgrade/page.tsx
@@ -2,26 +2,13 @@ import PricingPage from '@/components/PricingPage';
 import { currentUser } from '@clerk/nextjs/server';
 import React from 'react';
 
-// Use getServerSideProps for server-side data fetching
-export const getServerSideProps = async () => {
+// This is a server component
+const Page = async () => {
   const user = await currentUser(); // Fetch the current user server-side
 
-  // Pass the user ID to the page
-  return {
-    props: {
-      userId: Number(user?.id) || 0,
-    },
-  };
-};
-
-interface PageProps {
-  userId: number;
-}
-
-const Page: React.FC<PageProps> = ({ userId }) => {
   return (
     <div>
-      <PricingPage userId={userId.toString()} />
+      <PricingPage userId={(user?.id || '0').toString()} />
     </div>
   );
 };
